47408a0742ea5c94be96a8bdec6eb49c16ababa3
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-03-22 16:20:56 -0700

    Project Complete

--- Day 2/Tip_Calculator.py
+++ Day 2/Tip_Calculator.py
@@ -2,12 +2,11 @@
 
 print("Welcome to the Tip Calculator")
 
+# Inputs for needed values
 total = float(input("What was the total bill \n"))
-
 tip = (total * (0.01 * int(input("What percentage tip would you like to give? 10, 12, 15, or custom? \n"))))
-
 split = int(input("How many people are you splitting the bill with\n"))
 
+# Logic for Individual Costs
 individual_Cost = "{:.2f}".format((total + tip) / split)
-
 print(f"Each person should pay: ${individual_Cost}")
