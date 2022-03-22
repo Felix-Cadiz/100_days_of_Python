4691d6f6e0be457dff9ba09a7f55ba4d1e337fe6
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-03-22 16:22:47 -0700

    Project Complete

--- Day 1/Band_Name_Generator.py
+++ Day 1/Band_Name_Generator.py
@@ -1,8 +1,9 @@
-# Day 1 of Angela Yu's "100 days of Python" on udemy"
-
+# Day 1 of Angela Yu's "100 days of Python" on udemy
 print("Welcome to the band name generator!")
 
+# Inputs needed for name generator
 city = input("What's the name of the city you grew up in? \n")
 pet = input("What's your pet's name\n")
 
+# Band Name Generator Logic
 print("Your band name is" + city + " " + pet + "!")
