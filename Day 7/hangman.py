94cab2fa30d2791ca8747422646e5171e3ca4509
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-03-31 21:19:48 -0700

    initial commit

---
+++ Day 7/hangman.py
@@ -0,0 +1,17 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+
+print(chosen_word)
+
+guess = input("Guess a letter:\n").lower()
+
+print(guess)
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Correct")
+    else:
+        print("Incorrect")
