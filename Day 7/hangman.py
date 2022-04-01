8f1bb714529e0bc30696fd7302fcd0be14f557b3
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-04-01 15:47:44 -0700

    Game over occurs when the word is guessed correctly

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -5,21 +5,24 @@ random_word = random.choice(word_list)
 print(f"The answer is {random_word}")
 display = []
 lives = 5
+game_over = False
 
 word_length = len(random_word)
 for space in range(word_length):
     display += "_"
 print(display)
 
-# if random_word == display:
-#     print("You win!")
+while not game_over:
+    guess = input("Guess a letter:\n").lower()
+    # print(guess)
 
-guess = input("Guess a letter:\n").lower()
-# print(guess)
+    for index in range(word_length):
+        letter = random_word[index]
+        if letter == guess:
+            display[index] = letter
 
-for index in range(word_length):
-    letter = random_word[index]
-    if letter == guess:
-        display[index] = letter
+    print(display)
 
-print(display)
+    if "_" not in display:
+        game_over = True
+        print("You win! Game Over!")
