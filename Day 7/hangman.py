95fa64f8678d3d5f770ef06d09873e468ed9b392
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-04-02 06:19:18 -0700

    base functionality complete

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -1,11 +1,69 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 random_word = random.choice(word_list)
-print(f"The answer is {random_word}")
-display = []
-lives = 5
 game_over = False
+display = []
+lives = 6
+
+# print(f"The answer is {random_word}")
 
 word_length = len(random_word)
 for space in range(word_length):
@@ -14,15 +72,22 @@ print(display)
 
 while not game_over:
     guess = input("Guess a letter:\n").lower()
-    # print(guess)
 
     for index in range(word_length):
         letter = random_word[index]
         if letter == guess:
             display[index] = letter
+    
+    if guess not in display:
+        lives -= 1
 
-    print(display)
+    print(stages[lives])
+    print(f"{' '.join(display)}")
+
+    if lives == 0:
+        game_over = True
+        print("You lose! =(\nGame Over!")
 
     if "_" not in display:
         game_over = True
-        print("You win! Game Over!")
+        print("You win! =)\nGame Over!")
