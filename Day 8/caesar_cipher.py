7379568d85088d19c1b1826add54a83c6a006e91
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-04-04 16:49:25 -0700

    initial commit

---
+++ Day 8/caesar_cipher.py
@@ -0,0 +1,12 @@
+type = input("Type 'encode' to encrypt, type 'decode' to decrpt:\n").lower()
+shift = int(input("Type the shift number:\n"))
+result = ""
+
+# if (type == "encode"):
+#     print(type)
+
+# elif (type == "decode"):
+#     print(type)
+
+# else:
+#     print("PleaseTry again")
