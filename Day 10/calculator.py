645cc347610ab0561b86018ac7d69379b33c05ee
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-04-10 22:13:06 -0700

    initial commit

---
+++ Day 10/calculator.py
@@ -0,0 +1,13 @@
+# Day 10 of Angela Yu's "100 Days of Python" on udemy.
+
+# from art import logo
+
+first_digit = int(input("What's the first number?:\n"))
+
+operation = input("Pick an operation: + - * /: \n")
+
+second_digit = int(input("What's the next number?: \n"))
+
+# computation = (first_digit)(operation)(second_digit)
+
+print(first_digit (operation) (second_digit) )
