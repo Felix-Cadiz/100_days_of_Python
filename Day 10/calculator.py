e8d8beb4edc54dfc3f2b4266c8e350c7e7fd9090
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-04-12 22:03:45 -0700

    continuous calculation is functioning

--- Day 10/calculator.py
+++ Day 10/calculator.py
@@ -1,13 +1,52 @@
 # Day 10 of Angela Yu's "100 Days of Python" on udemy.
 
-# from art import logo
+from art import logo
 
+print(logo)
+calculate = True
+
+# Math Functions
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+def subtract(num1, num2):
+    difference = num1 - num2
+    return difference
+
+def multiply(num1, num2):
+    product = num1 * num2
+    return product
+
+def divide(num1, num2):
+    quotent = num1 / num2
+    return quotent
+
+def more_calculations(response):
+    if not response == "Y":
+        calculate = False
+
+# Calulator Logic
 first_digit = int(input("What's the first number?:\n"))
+# response = input(f"Type 'Y' to continue calculating with {final}, or type 'N' to start a new calculation.").upper()
 
-operation = input("Pick an operation: + - * /: \n")
-
-second_digit = int(input("What's the next number?: \n"))
-
-# computation = (first_digit)(operation)(second_digit)
-
-print(first_digit (operation) (second_digit) )
+while calculate == True:
+    operation = input("Pick an operation: + - * /: \n")
+    second_digit = int(input("What's the next number?: \n"))
+    if operation == "+":
+        final = add(first_digit, second_digit)
+        print(final)
+        first_digit = final
+        # more_calculations()
+    elif operation == "-":
+        final = subtract(first_digit, second_digit)
+        print(final)
+        first_digit = final
+    elif operation == "*":
+        final = multiply(first_digit, second_digit)
+        print(final)
+        first_digit = final
+    elif operation == "/":
+        final = divide(first_digit, second_digit)
+        print(final)
+        first_digit = final
