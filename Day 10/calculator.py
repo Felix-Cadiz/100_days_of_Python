48898523c2feefd45554a1ba913eba34e46cd171
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-04-13 09:08:14 -0700

    Project Complete

--- Day 10/calculator.py
+++ Day 10/calculator.py
@@ -1,49 +1,52 @@
 # Day 10 of Angela Yu's "100 Days of Python" on udemy.
 
 from art import logo
+import os
 
 print(logo)
-calculate = True
 
 # Math Functions
 def add(num1, num2):
-    sum = num1 + num2
-    return sum
+    return num1 + num2
 
 def subtract(num1, num2):
-    difference = num1 - num2
-    return difference
+    return num1 - num2
 
 def multiply(num1, num2):
-    product = num1 * num2
-    return product
+    return num1 * num2
 
 def divide(num1, num2):
-    quotent = num1 / num2
-    return quotent
+    return num1 / num2
+
+operations = { 
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
 
 # Calulator Logic
-first_digit = int(input("What's the first number?:\n"))
+def calculator():
+    calculate = True
+    num1 = float(input("What's the first number?:\n"))
+    while calculate:
+        operation_symbol = input("Pick an operation: + - * /: \n")
+        num2 = float(input("What's the next number?: \n"))
+        calculate_function = operations[operation_symbol]
+        answer = calculate_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        # For future calculations
+        num1 = answer
+        response = input(f"Type 'Y' to continue calculating with {answer}, type 'N' to start a new calculation, or anything else to exit this application.\n").upper()
+        if response == "Y":
+            num1 = answer
+        elif response == "N":
+            calculate = False
+            os.system("clear")
+            calculator()
+        else:
+            calculate = False
+            os.system("clear")
 
-while calculate == True:
-    operation = input("Pick an operation: + - * /: \n")
-    second_digit = int(input("What's the next number?: \n"))
-    if operation == "+":
-        final = add(first_digit, second_digit)
-        print(final)
-        first_digit = final
-    elif operation == "-":
-        final = subtract(first_digit, second_digit)
-        print(final)
-        first_digit = final
-    elif operation == "*":
-        final = multiply(first_digit, second_digit)
-        print(final)
-        first_digit = final
-    elif operation == "/":
-        final = divide(first_digit, second_digit)
-        print(final)
-        first_digit = final
-    response = input(f"Type 'Y' to continue calculating with {final}, or type 'N' to start a new calculation.\n").upper()
-    if response == "N":
-        calculate = False
+calculator()
