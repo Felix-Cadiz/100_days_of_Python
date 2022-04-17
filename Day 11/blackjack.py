911c137ff3d3c00d7fa00ae281c387be8ad233dd
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-04-16 20:55:06 -0700

    player total does not compute properly

--- Day 11/blackjack.py
+++ Day 11/blackjack.py
@@ -6,45 +6,51 @@ from art import logo
 import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-hands = {
-    'player': [],
-    'computer': [],
-}
-
-computer_total = sum(hands["computer"])
+player = []
+computer = []
+bust = False
 
 def restart():
     play_again = input("Do you want to play again? Type 'Y' or 'N':\n").upper()
     if play_again == "Y":
         os.system("clear")
-        hands = {
-            'player': [],
-            'computer': [],
-        }
+        print(logo)
+        player = []
+        computer = []
         blackjack()
     else: 
         os.system("clear")
         sys.exit()
 
-def bust(player_total):
-    if player_total < 21:
+def bust(total):
+    if total < 22:
         return
-    print(f"BUST! You went over 21!\nYour hand total was {player_total}")
-    restart()
+    bust = True
+    return bust, total
 
-def add_card_function(hands):
-    hands['player'].append(random.choice(cards))
-    player_total = sum(hands['player'])
-    bust(player_total)
-    for card in hands["player"]:
-        if card == 11 and player_total > 22:
-            card = 1
-    print(f"Your current hand is: {hands['player']} for a total of {player_total}")
-    return hands['player'], player_total
+def deal_card():
+    card = random.choice(cards)
+    return card
 
-def winner(hands):
-    player_total = sum(hands["player"])
-    computer_total = sum(hands['computer'])
+def ace(hand):
+    print(hand)
+    if 11 in hand and sum(hand) > 21:
+        hand.remove(11)
+        hand.append(1)
+        print(hand)
+        return sum(hand)
+
+def deal(player, player_total):
+    player.append(random.choice(cards))
+    player_total = sum(player)
+    # player_total = ace(player)
+    if bust(player_total):
+        print (f"BUST! You went over 21!")
+        restart()
+    print(f"Your current hand is: {player} for a total of {player_total}")
+    return player, player_total
+
+def winner(player_total, computer_total):
     if player_total > computer_total:
         return "You! Congratulations!"
     elif player_total == computer_total:
@@ -53,24 +59,27 @@ def winner(hands):
         return "The Computer!\nBetter luck next time!"
 
 def blackjack(): 
-    hands = {
-        'player': [random.choice(cards), random.choice(cards)],
-        'computer': [random.choice(cards), random.choice(cards)],
-    }
-    player_total = sum(hands['player'])
-    print(f"You cards are: {hands['player']}", "total =", player_total)
-    print(f"Computer's first card: {hands['computer'][0]}")
+    player = [deal_card(), deal_card()]
+    computer = [deal_card(), deal_card()]
+    player_total = sum(player)
+    computer_total = sum(computer)
+    print(f"You cards are: {player}, totalling {player_total}")
+    print(f"Computer's first card: {computer[0]}")
     add_card = input("Type 'y' to get another card, type 'n' to keep your hand:\n").upper()
     while add_card == "Y":
-        add_card_function(hands)
+        deal(player, player_total)
         add_card = input("Type 'y' to get another card, type 'n' to keep your hand:\n").upper()
-    # print(f"Your final hand: {player_hand}")
-    # print(f"The computer's final hand: {computer_hand}")
-    # if computer_total <= 15:
-    #     print("The computer needs more cards")
-    # print("Computer's final hand: {computer_hand}")
-
-    print(f"The winner is {winner(hands)}")
+    print(f"With a total of {player_total}, your final hand is {player}")
+    while computer_total < 17:
+        computer.append(deal_card())
+        computer_total = sum(computer)
+        if bust(computer_total):
+            print(f"With a total of {computer_total}, the computer's final hand is {computer}")
+            print (f"BUST! The computer went over 21!")
+            restart()
+    print(f"With a total of {computer_total}, the computer's final hand is {computer}")
+    print(f"The winner is {winner(player_total, computer_total)}")
+    print("player", player_total, "computer", computer_total)
     restart()
 
 start = input("Do you want to play a game of Blackjack? Type 'Y' or 'N':\n").upper()
