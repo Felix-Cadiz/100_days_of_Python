ef93e6b193408a17970fc3c8473891780f3028f2
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-04-22 18:33:22 -0700

    implemented game reset

--- Day 12/guessing_game.py
+++ Day 12/guessing_game.py
@@ -1,48 +1,60 @@
 # Day 12 of Angela Yu's "100 Days of Python" on udemy.
 
-from multiprocessing.connection import answer_challenge
 from random import randint
+from art import logo
+import os
 
-guesses = 0
-
+# Game Setup
+print(logo)
 print("Welcome to the Number Guessing Game!\nI am thinking of a number between 1 and 100")
-level = input("Choose a difficulty. Type 'easy' or 'hard':")
+EASY_MODE = 10
+HARD_MODE = 5
 
 def difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard':\n")
     if level == "easy":
-        guesses = 10
-        return guesses
-    elif level == "hard":
-        guesses = 5
-        return guesses
+        return EASY_MODE
     else:
-        print("Please input a valid response")
-        # level = input("Choose a difficulty. Type 'easy' or 'hard':")
-        # difficulty()
+        level == "hard"
+        return HARD_MODE
+
 
 guesses = difficulty()
-print(guesses)
-
 answer = randint(1, 100)
-print(answer)
 
-print(f"You have {guesses} attempts remaining.")
-attempt = int(input("Make a guess:\n"))
+# Game Logic
+def game():
+    global guesses
+    print(f"You have {guesses} attempts remaining.")
+    attempt = int(input("Make a guess:\n"))
+    while not attempt == answer and guesses > 1:
+        if attempt < answer: 
+            guesses -= 1
+            print(f"Too low.\nYou have {guesses} attempts remaining.")
+            attempt = int(input("Make a guess:\n"))
+        elif attempt > answer: 
+            guesses -= 1
+            print(f"Too high.\nYou have {guesses} attempts remaining.")
+            attempt = int(input("Make a guess:\n"))
 
-while not attempt == answer and guesses > 1:
-    if attempt < answer:
-        guesses -= 1
-        print("Too low.")
-        print(f"You have {guesses} attempts remaining.")
-        attempt = int(input("Make a guess:\n"))
-    elif attempt > answer:
-        guesses -= 1
-        print("Too high")
-        print(f"You have {guesses} attempts remaining.")
-        attempt = int(input("Make a guess:\n"))
+    if not attempt == answer and guesses == 1:
+        print(f"You lose!\nThe answer was {answer}")
 
-if not attempt == answer and guesses == 1:
-    print("You lose!")
+    if attempt == answer:
+        print(f"You got it! The answer was {answer}")
 
-if attempt == answer:
-    print(f"You got it! The answer was {answer}")
+def reset():
+    global guesses
+    global answer
+    play_again = input("Would you like to play again? 'Y' or 'N':\n").upper()
+    if play_again == "Y":
+        guesses = difficulty()
+        answer = randint(1, 100)
+        game()
+        reset()
+    else:
+        os.system("clear")
+
+game()
+
+reset()
